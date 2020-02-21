c5d04c12aa6bbf5099a71d54e4d318055b2e7a3a
Carlos Estrada <carlos.deg02@hotmail.com>
2020-02-21 11:19:26 -0600

    Added sum and sub

--- hw4/Matrices.c
+++ hw4/Matrices.c
@@ -6,16 +6,27 @@ void readMatrix(const char *message, int matrix[SIZE][SIZE]);
 
 void printMatrix(const char *message, int matrix[SIZE][SIZE]);
 
+void sumMatrices(int aMatrix[SIZE][SIZE], int bMatrix[SIZE][SIZE], int rMatrix[SIZE][SIZE]);
+
+void subtractMatrices(int aMatrix[SIZE][SIZE], int bMatrix[SIZE][SIZE], int rMatrix[SIZE][SIZE]);
+
 int main(void) 
 {
     int aMatrix[SIZE][SIZE];
     int bMatrix[SIZE][SIZE];
+    int rMatrix[SIZE][SIZE];
 
     readMatrix("Please enter values for matrix A", aMatrix);
     readMatrix("Please enter values for matrix B", bMatrix);
 
     printMatrix("Matrix A", aMatrix);
     printMatrix("Matrix B", bMatrix);
+
+    sumMatrices(aMatrix, bMatrix, rMatrix);
+    printMatrix("Sum of A and B", rMatrix);
+
+    subtractMatrices(aMatrix, bMatrix, rMatrix);
+    printMatrix("Subtraction of A and B", rMatrix);
 }
 
 void readMatrix(const char *message, int matrix[SIZE][SIZE]) 
@@ -43,5 +54,41 @@ void printMatrix(const char *message, int matrix[SIZE][SIZE])
         }
 
         printf("\n");
+    }
+}
+
+void sumMatrices(int aMatrix[SIZE][SIZE], int bMatrix[SIZE][SIZE], int rMatrix[SIZE][SIZE]) 
+{
+    for(int i = 0; i < SIZE; i++) 
+    {
+        for(int j = 0; j < SIZE; j++)
+        {
+            rMatrix[i][j] = aMatrix[i][j] + bMatrix[i][j];        
+        }
+    }
+}
+
+void subtractMatrices(int aMatrix[SIZE][SIZE], int bMatrix[SIZE][SIZE], int rMatrix[SIZE][SIZE]) 
+{
+    for(int i = 0; i < SIZE; i++) 
+    {
+        for(int j = 0; j < SIZE; j++)
+        {
+            rMatrix[i][j] = aMatrix[i][j] - bMatrix[i][j];        
+        }
+    }
+}
+
+void multipluMatrices(int aMatrix[SIZE][SIZE], int bMatrix[SIZE][SIZE], int rMatrix[SIZE][SIZE]) 
+{
+    for(int i = 0; i < SIZE; i++) 
+    {
+        for(int j = 0; j < SIZE; j++)
+        {
+            for(int k = 0; k < SIZE; k++)
+            {
+                  
+            }   
+        }
     }
 }
